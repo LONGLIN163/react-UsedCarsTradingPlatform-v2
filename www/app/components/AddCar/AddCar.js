fbbc7bd9021d6ae2d1be7dfb7e13b216371ef718
developerlin123 <developerlin123@gmail.com>
2022-03-24 17:35:39 +0100

    fixed uploads

--- www/app/components/AddCar/AddCar.js
+++ www/app/components/AddCar/AddCar.js
@@ -15,7 +15,7 @@ class AddCar extends React.Component {
     super(props)
     this.getImagesObj=this.getImagesObj.bind(this)
     this.state = {
-      current: 3,
+      current: 1,
     };
   }
 
