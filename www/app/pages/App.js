ca16535ecf977cff2e169d71e0f8d9e356ac8a72
developerlin123 <developerlin123@gmail.com>
2022-04-05 14:09:33 +0200

    temp version first

--- www/app/pages/App.js
+++ www/app/pages/App.js
@@ -28,16 +28,16 @@ class App extends React.Component {
                   <Menu.Item key="/sale/addcar">Sell Your Car</Menu.Item>
                 </Menu>
               </Header>
-              <Row className="comm-main" type="flex" justify="center">
-        {/* <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
-          <div>
-              <Breadcrumb.Item>
-                <Link href="/"><a>Home</a></Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>{results.data[0].typeName ? results.data[0].typeName : "other"}</Breadcrumb.Item>
-          </div>
-        </Col> */}
-      </Row>
+              {/* <Row className="comm-main" type="flex" justify="center">
+                <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
+                  <div>
+                      <Breadcrumb.Item>
+                        <Link href="/"><a>Home</a></Link>
+                      </Breadcrumb.Item>
+                      <Breadcrumb.Item>{results.data[0].typeName ? results.data[0].typeName : "other"}</Breadcrumb.Item>
+                  </div>
+                </Col>
+              </Row> */}
               {this.props.children}
           </Layout>
       </div>
